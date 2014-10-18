3b25cfb01e09f4b4d668a63a41df317851c76406
Andrew Sage <andrew@asae.co.uk>
2014-10-18 12:30:58 +0100

    EffortRating rating validation added.

--- app/models/effort_rating.rb
+++ app/models/effort_rating.rb
@@ -1,3 +1,4 @@
 class EffortRating < ActiveRecord::Base
   belongs_to :opportunity
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
