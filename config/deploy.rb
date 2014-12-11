b908edad17ea886c137feaf51dcd048d2433f78c
Andrew Sage <andrew@asae.co.uk>
2014-12-11 23:13:06 +0000

    Location of Northfield Swimming Pool corrected.

--- config/deploy.rb
+++ config/deploy.rb
@@ -78,6 +78,10 @@ namespace :deploy do
     run_remote_rake "import:rebuild_all"
   end
 
+  task :rebuild_venues, roles: :app do
+    run_remote_rake "import:venues"
+  end
+
   task :rebuild_asv, roles: :app do
     run_remote_rake "import:asv_classes_json"
     run_remote_rake "import:asv_swimming_classes_json"
