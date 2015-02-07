8d668a54871e9ea25280f2531f30ef72ef720867
Andrew Sage <andrew@asae.co.uk>
2015-02-07 15:04:57 +0000

    Production email server configured. User account links added.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,13 @@ Rails.application.configure do
 
 
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => <%= ENV['MATCHTHECITY_EMAIL'],
+    :password             => <%= ENV['MATCHTHECITY_EMAIL_PASSWORD'],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
   config.action_mailer.default_url_options = {:host => "matchthecity.org" }
 end
